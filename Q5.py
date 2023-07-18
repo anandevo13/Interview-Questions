8a2dd391c7c0653c70bca6efa0164341a67c5c77
anandevo13 <100212329+anandevo13@users.noreply.github.com>
2023-07-18 17:38:03 +0530

    Add files via upload

---
+++ Q5.py
@@ -0,0 +1,23 @@
+"""
+Q. Print a program for a list of numbers which when divisible by 3 gives fizz, divisible by 5 gives buzz 
+else returns num.
+"""
+
+#for loop that traverses numbers from 1 to 100
+for i in range(1,101):
+  #check if number is divisible by both 3 and 5
+  if(i%3==0 and i%5==0):
+    print("FizzBuzz")
+  #check if number is divisible by 3
+  elif(i%3 == 0):
+    print("Fizz")
+  #check if number is divisible by 5
+  elif(i%5 == 0):
+    print("Buzz")
+  #if not divisible by either of them print the i
+  else:
+    print(i)  
+        
+        
+        
+        
